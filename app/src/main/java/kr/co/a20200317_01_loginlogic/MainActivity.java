b929ae36b7440be06eb7490de1205b9b52db647d
tjddls1728 <61854982+tjddls1728@users.noreply.github.com>
2020-03-17 10:03:27 +0900

    로그인화면 구현

---
+++ app/src/main/java/kr/co/a20200317_01_loginlogic/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.co.a20200317_01_loginlogic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
